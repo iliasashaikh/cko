2cb403604f7b9db865293f8c21917a05ee44499f
iliasashaikh <iliasashaikh@gmail.com>
2021-12-28 12:52:16 +0000

    More integration tests

--- Cko.PaymentGateway/Tests/Cko.PaymentGateway.IntegrationTests2/PaymentControllerIntTests.cs
+++ Cko.PaymentGateway/Tests/Cko.PaymentGateway.IntegrationTests2/PaymentControllerIntTests.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Cko.PaymentGateway.IntegrationTests2
 {
@@ -19,12 +21,20 @@ namespace Cko.PaymentGateway.IntegrationTests2
         [OneTimeSetUp]
         public void CreateTestFactory()
         {
-            _application = new WebApplicationFactory<Program>();
-            _client = _application.WithWebHostBuilder(builder => 
+            _application = new WebApplicationFactory<Program>()
+                                .WithWebHostBuilder(builder =>
             {
-                builder.ConfigureAppConfiguration
+                builder.ConfigureAppConfiguration((c, b) =>
+                {
+                    var p = b.Properties;
+                    var projectDir = Directory.GetCurrentDirectory();
+                    var configPath = Path.Combine(projectDir, "appsettings.json");
+
+                    b.AddJsonFile(configPath);
+                });
             });
 
+            _client = _application.CreateClient();
             _merchantRepo = (MerchantRepository) _application.Services.GetService(typeof(MerchantRepository));
 
         }
@@ -34,6 +44,16 @@ namespace Cko.PaymentGateway.IntegrationTests2
         {
             var paymentReq = new PaymentRequest();
             var response = await _client.PostAsJsonAsync("api/Payments",paymentReq);
+            Assert.AreEqual(HttpStatusCode.UnprocessableEntity, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Test_PaymentWithValidMerchant()
+        {
+            var id = await _merchantRepo.Insert(new Entities.Merchant() {Name="a", Address="address 2" });
+
+            var paymentReq = new PaymentRequest() { MerchantId = id };
+            var response = await _client.PostAsJsonAsync("api/Payments", paymentReq);
             Assert.AreEqual(HttpStatusCode.UnprocessableEntity, response.StatusCode);
         }
     }
